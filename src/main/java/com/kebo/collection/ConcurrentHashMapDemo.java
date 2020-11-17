08e7087edaf3d2c19114bd55090550ade3d76deb
kebo <249183617@qq.com>
2020-11-17 19:37:13 +0800

    加一些东西

---
+++ src/main/java/com/kebo/collection/ConcurrentHashMapDemo.java
@@ -0,0 +1,21 @@
+package com.kebo.collection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @description:
+ * @author: kb
+ * @create: 2020-11-14 16:32
+ **/
+public class ConcurrentHashMapDemo {
+    public static void main(String[] args) {
+        ConcurrentLinkedQueue concurrentLinkedQueue=new ConcurrentLinkedQueue();
+    }
+}
+
